31132d470211067ceb34b144008f40815f0d0657
alezalez8 <5446943@ukr.net>
2021-02-25 21:57:41 +0200

    HW2

--- src/urise/webapp/storage/ArrayStorage.java
+++ src/urise/webapp/storage/ArrayStorage.java
@@ -33,48 +33,43 @@ public class ArrayStorage implements Storage {
     }
 
     public void save(Resume resume) {
-        if (isPresent(resume.getUuid())) {
-            System.out.println("ERROR: this record already present in base");
-
+        if (getIndex(resume.getUuid()) != -1) {
+            System.out.println("Resume " + resume.getUuid() + " already exist");
+        } else if (size == storage.length) {
+            System.out.println("Storage overflow ");
         } else {
-            if (size < storage.length) {
-                storage[size] = resume;
-                size++;
-            }
+            storage[size] = resume;
+            size++;
         }
-
     }
 
 
     public void update(Resume resume) {
-        if (isPresent(resume.getUuid())) {
-            System.out.println("urise.webapp.model.Resume is present in base, input another name");
-        }
-
+        int index = getIndex(resume.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + resume.getUuid() + " not exist");
+        } else
+            storage[index] = resume;
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid == storage[i].getUuid()) {
-                return storage[i];
-            } else System.out.println("ERROR");
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
         }
-        return null;
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid == storage[i].getUuid()) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                break;
-
-            } else System.out.println("ERROR");
-
-
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
-
     }
 
 
